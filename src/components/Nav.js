860b70af087f3af72cfa533e4ece6143e4bc1ad1
Eiqko <sarandaeiko@gmail.com>
2020-09-12 00:13:08 +0700

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return (
+        <nav style={{ background: '#2E3B55' }}>
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo">Carifilm Centratama</a>                
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
